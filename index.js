271fcafc223127d7efec5d7aba6105cad980dfd8
Martin <martinbutts@clear.net.nz>
2016-12-02 11:47:42 +1300

    add transform for box2

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ document.addEventListener('click', function(ev){
     f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
     f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
 },false);
+
+$('.trigger').on('click', function() {
+  $(this).toggleClass('clicked');
+});
